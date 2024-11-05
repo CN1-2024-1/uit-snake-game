6eb6d2bd90f8cadc59bbe46bea91004724ba1c73
trquang0810 <42947649+trquang0810@users.noreply.github.com>
2024-11-05 20:06:49 +0700

    them mau sau cho food...

--- CODE/main.cpp
+++ CODE/main.cpp
@@ -4,6 +4,7 @@ class Food
 private:
 	int x, y;
     char symbol; // Bieu tuong thuc an
+	int color; // Mau thuc an
 public:
 	Food() 
 	{
@@ -15,8 +16,11 @@ public:
         x = rand() % (width - 2) + 1;
         y = rand() % (height - 2) + 1;
         symbol = 'A' + rand() % 26;
+		color = 10 + rand() % 7; // Mau ngau nhien (10 - 16)
     }
 	
     int getX() const { return x; }
     int getY() const { return y; }
+	char getSymbol() const { return symbol; }
+    int getColor() const { return color; }
 };
