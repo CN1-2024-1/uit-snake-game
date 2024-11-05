3676c5d9a7ee29405817f17addf76539f9500606
nguyenhackathon <nguyenhanguyen.it@gmail.com>
2024-11-05 19:52:59 +0700

    add load and save highest score

--- CODE/main.cpp
+++ CODE/main.cpp
@@ -16,7 +16,29 @@ void setColor(int color) {
     SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), color);
 }
 
+// Function to load the highest score from a file
+int loadHighestScore() {
+    int score = 0;
+    ifstream inFile(highScoreFile);
+    if (inFile.is_open()) {
+        inFile >> score;
+        inFile.close();
+    }
+    return score;
+}
+
+// Function to save the highest score to a file
+void saveHighestScore(int score) {
+    ofstream outFile(highScoreFile);
+    if (outFile.is_open()) {
+        outFile << score;
+        outFile.close();
+    }
+}
+
+// Main function where the game runs
 int main()
 {
+    int highestScore = loadHighestScore(); // Load highest score from file
     return 0;
 }
