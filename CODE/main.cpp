5299c647489587b27ea785fcbe9a4f9239bd9103
trquang0810 <42947649+trquang0810@users.noreply.github.com>
2024-11-05 20:00:53 +0700

    Update main.cpp

--- CODE/main.cpp
+++ CODE/main.cpp
@@ -2,8 +2,21 @@
 class Food 
 {
 private:
-
-
+	int x, y;
+    char symbol; // Bieu tuong thuc an
 public:
+	Food() 
+	{
+        spawn();
+    }
 
+    void spawn() 
+	{
+        x = rand() % (width - 2) + 1;
+        y = rand() % (height - 2) + 1;
+        symbol = 'A' + rand() % 26;
+    }
+	
+    int getX() const { return x; }
+    int getY() const { return y; }
 };
