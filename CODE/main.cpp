472aa05a8ee9742ed95332086a075a26e1b1e9d1
nguyenhackathon <nguyenhanguyen.it@gmail.com>
2024-11-05 19:51:08 +0700

    create setColor func and main func

--- CODE/main.cpp
+++ CODE/main.cpp
@@ -10,3 +10,13 @@ using namespace std;
 
 const int width = 50; // Width of the game board
 const int height = 20; // Height of the game board
+
+// Function to set text color
+void setColor(int color) {
+    SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), color);
+}
+
+int main()
+{
+    return 0;
+}
