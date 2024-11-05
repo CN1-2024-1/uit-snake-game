c4b4381905e7665ef40cd1d00c80614d66bc5a09
nguyenhackathon <nguyenhanguyen.it@gmail.com>
2024-11-05 19:48:49 +0700

    Create main.cpp

---
+++ CODE/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <conio.h> // For _kbhit() and _getch()
+#include <windows.h> // For Sleep() and SetConsoleTextAttribute() and Beep()
+#include <vector>
+#include <cstdlib> // For rand() and srand()
+#include <ctime> // For time()
+#include <fstream> // For file handling
+
+using namespace std;
+
+const int width = 50; // Width of the game board
+const int height = 20; // Height of the game board
